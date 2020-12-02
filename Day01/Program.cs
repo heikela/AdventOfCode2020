611136814c4587210db99d414b05f80251ef9891
Mikko Heikelä <mikko.heikela@gmail.com>
2020-12-02 05:21:21 +0000

    Add solutions for days 1 and 2

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace Day01
+
+namespace AdventOfCode2020
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> input = File.ReadLines("../../../input.txt").Select(s => int.Parse(s)).ToList();
+
+            for (int i = 0; i < input.Count - 1; ++i)
+            {
+                for (int j = i + 1; j < input.Count; ++j)
+                {
+                    int first = input[i];
+                    int second = input[j];
+                    if (first + second == 2020)
+                    {
+                        Console.WriteLine(first * second);
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < input.Count - 2; ++i)
+            {
+                for (int j = i + 1; j < input.Count - 1; ++j)
+                {
+                    for (int k = j + 1; k < input.Count; ++k)
+                    {
+                        int first = input[i];
+                        int second = input[j];
+                        int third = input[k];
+                        if (first + second + third == 2020)
+                        {
+                            Console.WriteLine(first * second * third);
+                        }
+                    }
+                }
+            }
         }
     }
 }
+
---
+++ Day02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Day02
+{
+    class Program
+    {
+
+        public struct Item
+        {
+            public int Min;
+            public int Max;
+            public Char Letter;
+            public String Password;
+        }
+
+        public static Item Parse(string line)
+        {
+            var pwdMatch = Regex.Match(line, "(\\d+)-(\\d+) (\\w): (\\w+)");
+            if (!pwdMatch.Success)
+            {
+                throw new ArgumentException();
+            }
+            return new Item()
+            {
+                Min = int.Parse(pwdMatch.Groups[1].Value),
+                Max = int.Parse(pwdMatch.Groups[2].Value),
+                Letter = pwdMatch.Groups[3].Value[0],
+                Password = pwdMatch.Groups[4].Value
+            };
+        }
+
+        public static bool Check(Item pwdItem)
+        {
+            string password = pwdItem.Password;
+            Char letter = pwdItem.Letter;
+            int count = password.Count(c => c == letter);
+            return count >= pwdItem.Min && count <= pwdItem.Max;
+        }
+
+        public static bool Check2(Item pwdItem)
+        {
+            string password = pwdItem.Password;
+            Char letter = pwdItem.Letter;
+
+            return password[pwdItem.Min - 1] == letter ^ password[pwdItem.Max - 1] == letter;
+        }
+
+        static void Main(string[] args)
+        {
+            List<Item> items = File.ReadLines("../../../input.txt").Select(s => Parse(s)).ToList();
+            Console.WriteLine(items.Count(Check));
+            Console.WriteLine(items.Count(Check2));
+        }
+    }
+}
