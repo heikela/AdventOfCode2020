013cd410f8acb90ce8ff4ddec32a8cc414b06add
Mikko Heikelä <mikko.heikela@gmail.com>
2020-12-03 05:21:33 +0000

    Add day 3 solution

---
+++ Day03/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Day03
+{
+    public record Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y) => (X, Y) = (x, y);
+
+        public static Point operator +(Point a, Point b) => new Point(a.X + b.X, a.Y + b.Y);
+    }
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<bool>> trees = File.ReadLines("../../../input.txt").Select(s => s.ToCharArray().Select(c => c == '#').ToList()).ToList();
+            int count = CheckSlope(trees, new Point(3, 1));
+            Console.WriteLine(count);
+
+            List<Point> directions = new List<Point>() {
+                new Point(1, 1),
+                new Point(3, 1),
+                new Point(5, 1),
+                new Point(7, 1),
+                new Point(1, 2)
+            };
+
+            Console.WriteLine(directions.Select<Point, long>(dir => CheckSlope(trees, dir)).Aggregate((a, b) => a * b));
+        }
+
+        private static int CheckSlope(List<List<bool>> trees, Point dir)
+        {
+            int width = trees[0].Count;
+            Point pos = new Point(0, 0);
+            int count = 0;
+            while (pos.Y < trees.Count)
+            {
+                if (trees[pos.Y][pos.X % width])
+                {
+                    count++;
+                }
+                pos = pos + dir;
+            }
+
+            return count;
+        }
+    }
+}
