f943ca918519cab9d33b6d1356f05212709a188b
Mikko Heikelä <mikko.heikela@gmail.com>
2020-12-06 05:39:50 +0000

    Put SplitWhen into a common library as an extension method on IEnumerable

---
+++ Common/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace Common
+{
+    public static class Extensions
+    {
+        public static IEnumerable<List<TElem>> SplitWhen<TElem>(this IEnumerable<TElem> seq,
+            Func<TElem, TElem, bool> splitPredicate)
+        {
+            List<TElem> prefix = new List<TElem>();
+            foreach (TElem next in seq)
+            {
+                if (prefix.Count >= 1)
+                {
+                    if (splitPredicate(prefix.Last(), next))
+                    {
+                        yield return prefix;
+                        prefix = new List<TElem>();
+                    }
+                }
+                prefix.Add(next);
+            }
+            if (prefix.Any())
+            {
+                yield return prefix;
+            }
+            yield break;
+        }
+    }
+}
--- Day06/Program.cs
+++ Day06/Program.cs
@@ -2,43 +2,21 @@
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
+using Common;
 
 namespace Day06
 {
     class Program
     {
-        public static IEnumerable<List<TElem>> SplitWhen<TElem>(IEnumerable<TElem> seq,
-            Func<TElem, TElem, bool> splitPredicate)
-        {
-            List<TElem> prefix = new List<TElem>();
-            foreach (TElem next in seq)
-            {
-                if (prefix.Count >= 1)
-                {
-                    if (splitPredicate(prefix.Last(), next))
-                    {
-                        yield return prefix;
-                        prefix = new List<TElem>();
-                    }
-                }
-                prefix.Add(next);
-            }
-            if (prefix.Any())
-            {
-                yield return prefix;
-            }
-            yield break;
-        }
-
         static void Main(string[] args)
         {
-            int result = SplitWhen(File.ReadLines("../../../input.txt"), (p, n) => (n.Trim().Count() == 0))
+            IEnumerable<List<string>> grouped = File.ReadLines("../../../input.txt").SplitWhen((p, n) => n.Trim().Length == 0);
+            int result = grouped
                 .Select(g => string.Concat(g).ToHashSet().Count)
                 .Sum();
             Console.WriteLine(result);
 
-            int result2 = SplitWhen(File.ReadLines("../../../input.txt"), (p, n) => (n.Trim().Count() == 0))
+            int result2 = grouped
                 .Select(g => g.Where(s => s.Trim().Length > 0).Select(s => s.ToHashSet()).Aggregate((a, b) => { a.IntersectWith(b); return a; }).Count)
                 .Sum();
             Console.WriteLine(result2);
