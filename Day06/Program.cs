17d4cf813ff64a7c997081b811a47bf11d6f73a2
Mikko Heikelä <mikko.heikela@gmail.com>
2020-12-06 05:44:36 +0000

    Trim all lines early to simply further processing

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -10,14 +10,14 @@ namespace Day06
     {
         static void Main(string[] args)
         {
-            IEnumerable<List<string>> grouped = File.ReadLines("../../../input.txt").SplitWhen((p, n) => n.Trim().Length == 0);
+            IEnumerable<List<string>> grouped = File.ReadLines("../../../input.txt").Select(s => s.Trim()).SplitWhen((p, n) => n.Length == 0);
             int result = grouped
                 .Select(g => string.Concat(g).ToHashSet().Count)
                 .Sum();
             Console.WriteLine(result);
 
             int result2 = grouped
-                .Select(g => g.Where(s => s.Trim().Length > 0).Select(s => s.ToHashSet()).Aggregate((a, b) => { a.IntersectWith(b); return a; }).Count)
+                .Select(g => g.Where(s => s.Length > 0).Select(s => s.ToHashSet()).Aggregate((a, b) => { a.IntersectWith(b); return a; }).Count)
                 .Sum();
             Console.WriteLine(result2);
         }
