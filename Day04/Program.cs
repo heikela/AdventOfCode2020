bfe35651e9756dfab136c14fdfbe7130210fe31f
Mikko Heikelä <mikko.heikela@gmail.com>
2020-12-08 23:49:48 +0000

    Add day 4 part 2 work in progress

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 using Common;
+using System.Text.RegularExpressions;
 
 namespace Day04
 {
@@ -24,14 +25,55 @@ namespace Day04
             "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"
         };
 
-        static bool IsValidPassport(Dictionary<string, string> document)
+        static bool PassportHasRequiredFields(Dictionary<string, string> document)
         {
             return RequiredFields.All(field => document.ContainsKey(field));
+        }
+
+        static bool IsValidPassport(Dictionary<string, string> document)
+        {
+            if (!PassportHasRequiredFields(document))
+            {
+                return false;
+            }
+            foreach (var kv in document)
+            {
+                switch (kv.Key)
+                {
+                    case "byr":
+                        var match = Regex.Match(kv.Value, @"^(\d+)$");
+                        if (!match.Success)
+                        {
+                            return false;
+                        }
+                        int val = int.Parse(match.Groups[1].Value);
+                        if (val < 1920 | val > 2002)
+                        {
+                            return false;
+                        }
+                        break;
+                    case "iyr":
+                        break;
+                    case "eyr":
+                        break;
+                    case "hgt":
+                        break;
+                    case "hcl":
+                        break;
+                    case "ecl":
+                        break;
+                    case "pid":
+                        break;
+                    default: break;
+                }
+            }
+            return true;
         }
 
         static void Main(string[] args)
         {
             IEnumerable<Dictionary<string, string>> passports = File.ReadLines("../../../input.txt").Paragraphs().Select(ParsePassport);
+            Console.WriteLine(passports.Count(PassportHasRequiredFields));
             Console.WriteLine(passports.Count(IsValidPassport));
         }
     }
