2771e9582f3dd1dc1ff8f5580377e391eeace757
Mikko Heikelä <mikko.heikela@gmail.com>
2020-12-09 10:39:45 +0000

    Solve day 9 part 2

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -9,7 +9,7 @@ namespace Day09
     {
         static void Main(string[] args)
         {
-            IEnumerable<long> entries = File.ReadLines("../../../input.txt").Select(long.Parse);
+            List<long> entries = File.ReadLines("../../../input.txt").Select(long.Parse).ToList();
             Queue<long> available = new Queue<long>(entries.Take(25));
             IEnumerator<long> toCheck = entries.Skip(25).GetEnumerator();
 
@@ -41,7 +41,29 @@ namespace Day09
                 available.Dequeue();
                 available.Enqueue(toCheck.Current);
             }
-            Console.WriteLine(toCheck.Current);
+
+            long invalidNumber = toCheck.Current;
+            Console.WriteLine(invalidNumber);
+
+            // part 2
+
+            for (int i = 0; i < entries.Count(); ++i)
+            {
+                long sum = 0;
+                for (int j = i; j < entries.Count(); ++j)
+                {
+                    sum += entries[j];
+                    if (sum == invalidNumber)
+                    {
+                        IEnumerable<long> range = entries.Skip(i).Take(j - i + 1);
+                        Console.WriteLine(range.Min() + range.Max());
+                    }
+                    if (sum > invalidNumber)
+                    {
+                        break;
+                    }
+                }
+            }
         }
     }
 }
