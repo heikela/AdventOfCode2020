94e2f667288db086274b52d1579b36543a28a3e0
Mikko Heikelä <mikko.heikela@gmail.com>
2020-12-09 10:31:07 +0000

    Solve day 9 part 1

---
+++ Day09/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<long> entries = File.ReadLines("../../../input.txt").Select(long.Parse);
+            Queue<long> available = new Queue<long>(entries.Take(25));
+            IEnumerator<long> toCheck = entries.Skip(25).GetEnumerator();
+
+            bool IsValid(long number) {
+                List<long> sorted = available.AsEnumerable().OrderBy(x => x).ToList();
+                int lowIdx = 0;
+                int highIdx = sorted.Count() - 1;
+                while (lowIdx < highIdx)
+                {
+                    long sum = sorted[lowIdx] + sorted[highIdx];
+                    if (sum == number)
+                    {
+                        return true;
+                    }
+                    else if (sum < number)
+                    {
+                        ++lowIdx;
+                    }
+                    else
+                    {
+                        --highIdx;
+                    }
+                }
+                return false;
+            }
+
+            while (toCheck.MoveNext() && IsValid(toCheck.Current))
+            {
+                available.Dequeue();
+                available.Enqueue(toCheck.Current);
+            }
+            Console.WriteLine(toCheck.Current);
+        }
+    }
+}
